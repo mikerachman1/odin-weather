2435c0020be94bb8059e574df9aec6527160d0a5
Mike Rachman <mikerachman1@gmail.com>
2022-12-19 14:29:43 -0500

    added displayData function

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
 import './style.css'
 
 
-const returnWeatherPromise = async (location) => {
+const displayData = (data) => {
+  const temperature = data.main.temp;
+  const {humidity} = data.main;
+  const {description} = data.weather[0]
+  const windspeed = data.wind.speed
+  const placeName = `${data.name}, ${data.sys.country}`
+  console.log(temperature, humidity, description, windspeed, placeName)
+}
+
+const fetchWeatherData = async (location) => {
   try {
     const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${process.env.API_KEY}`, {mode: 'cors'});
     const searchData = await response.json()
     console.log(searchData)
+
+    await displayData(searchData)
+
+
   } catch (error) {
     console.log(error)
   }
 }
 
-returnWeatherPromise('london, uk')
+fetchWeatherData('London, uk')
+
+
+
+
 
