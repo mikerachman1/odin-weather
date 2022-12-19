3904cb3026ed6007f4d4647eeb2030a4d06fe223
Mike Rachman <mikerachman1@gmail.com>
2022-12-19 13:34:42 -0500

    returnWeatherData function written in index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import './style.css'
 
 
-console.log(process.env.API_KEY, process.env.WEATHER);
+const returnWeatherPromise = async (location) => {
+  try {
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${process.env.API_KEY}`, {mode: 'cors'});
+    const searchData = await response.json()
+    console.log(searchData)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+returnWeatherPromise('london, uk')
+
